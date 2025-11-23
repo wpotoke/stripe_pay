832c5061269da1d2a3e0cbd531ccd1e8b15dab69
moroz <morozovdaniel713@gmail.com>
2025-11-24 00:01:00 +0300

    refactor(core.urls): rename path

--- pay_stripe_system/urls.py
+++ pay_stripe_system/urls.py
@@ -18,4 +18,4 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("stripe/", include("store.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("store/", include("store.urls"))]
