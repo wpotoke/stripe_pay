5853cfb5ef0f4a459dbaad797dfe2166a6ed8951
moroz <morozovdaniel713@gmail.com>
2025-11-24 01:37:57 +0300

    feat(admin): regsiter models and display any params in admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 
-from store.models import Item
+from store.models import Discount, Item, Order, OrderItem, Tax
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("id", "name", "price", "currency")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "currency", "status", "created_at")
+    list_filter = ("status", "currency")
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin): ...
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin): ...
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin): ...
