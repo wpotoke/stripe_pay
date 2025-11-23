35d683711c5e0f247f5e0b215c71b623b746205e
moroz <morozovdaniel713@gmail.com>
2025-11-24 00:04:24 +0300

    feat(store.urls): add paths for item, success and session

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from store.views import get_session, index
+from store.views import get_item, get_session, stripe_success
 
 urlpatterns = [
-    path("start/", get_session, name="start"),
-    path("success/", index, name="index"),
+    path("buy/<int:item_id>/", get_session, name="get_session"),
+    path("item/<int:item_id>/", get_item, name="get_item"),
+    path("success/", stripe_success, name="stripe_success"),
 ]
